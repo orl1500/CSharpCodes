21b462718a5f32364b1c3c4617a23a8b18416fad
Orleans <Orleans@ORLEANS>
2017-09-21 20:14:45 -0300

    Primeiro Salvamento

---
+++ Another Test/Another Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_Test
+{
+
+    class program
+    {
+
+        static void Main (string [] args )
+        {
+
+            Console.WriteLine("Cópia de Nome Digitado - IMPUT");
+            Console.WriteLine("Digite o seu nome: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(" Seu nome é: " + name);
+
+            Console.ReadKey();
+
+        }
+        
+    }
+
+}
