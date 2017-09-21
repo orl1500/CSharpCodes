428feb99c80dd82cc7cbc522957fa863f6e3b134
Orleans <Orleans@ORLEANS>
2017-09-21 19:56:28 -0300

    Inseri comentários e infos para o início do programa

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -12,8 +12,12 @@ namespace Hello_World
 
         static void Main ( string [] args )
         {
+            //Informações na tela
+            Console.WriteLine(" Bem-vindo ");
+            Console.WriteLine(" Este é o programa mais básico da programação ");
 
             Console.WriteLine(" Hello World ");
+            Console.ReadLine(); // Ler a linha para fechar o programa
 
         
         }
