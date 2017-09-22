a9cedefb6ac71f4c597089656aca338c3423d7c4
Orleans <Orleans@ORLEANS>
2017-09-21 22:10:11 -0300

    Novo Documento

---
+++ CALCULADORA/CALCULADORA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+
+    class program
+    {
+
+        static void Main ( string [] args )
+        {
+
+            Console.WriteLine(" Calculadora - SOMA ");
+            Console.WriteLine(" Digite Aqui Seu Nome: ");
+
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine (  )
+
+
+        }
+
+    }
+
+}
