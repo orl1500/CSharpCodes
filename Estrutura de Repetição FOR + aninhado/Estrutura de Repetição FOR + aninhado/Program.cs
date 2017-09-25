efb5437ba76b560dbab37a6a6ff465e4afb74bb6
Orleans <Orleans@ORLEANS>
2017-09-25 08:44:07 -0300

    Estrutura completa do Laço For

---
+++ Estrutura de Repetição FOR + aninhado/Estrutura de Repetição FOR + aninhado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_Repetição_FOR___aninhado
+{
+
+    class program
+    {
+
+        static void Main ( string [] args )
+        {
+
+            Console.WriteLine(" Welcome to My Machine "); //Tela de apresentação do programa
+
+            for (int contagem = 5; contagem <= 10; contagem++ )
+            {
+
+                Console.WriteLine(" Olá ");
+
+            }
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+}
