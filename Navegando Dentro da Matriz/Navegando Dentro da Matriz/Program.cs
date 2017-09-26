2b1e9dbfb34b0ac4d7508bb266fe88435ea27e51
Orleans <Orleans@ORLEANS>
2017-09-26 14:11:31 -0300

    Termine o Programa

---
+++ Navegando Dentro da Matriz/Navegando Dentro da Matriz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navegando_Dentro_da_Matriz
+{
+
+    class program
+    {
+
+        static void Main ( string [] args )
+        {
+
+            Console.WriteLine(" \n\n \t\t\t\t\t Welcome To XList ");
+            Console.WriteLine(" \n\n \t\t\t\t Digite seu Nome: ");
+
+            string nome = Console.ReadLine();
+
+            string[] name = new string[4];
+            name[1] = "Orleans";
+            name
+
+        }
+
+    }
+
+}
