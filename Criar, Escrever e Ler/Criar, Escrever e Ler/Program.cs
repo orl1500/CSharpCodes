4e8deec1379ddf3c3440c62c28369bacdbfd69d6
Orleans <Orleans@ORLEANS>
2017-09-27 10:38:34 -0300

    Criação de Arquivo C#

---
+++ Criar, Escrever e Ler/Criar, Escrever e Ler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Criar_Escrever_e_Ler
+{
+
+    class program
+    {
+
+        static void Main ( string [] args )
+        {
+
+            Console.WriteLine(" \n\n \t\t\t\t\t\t ===Comandos Criar, Escrever e Ler===");
+
+            TextWriter text = new StreamWriter(" Write Here.txt ");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
